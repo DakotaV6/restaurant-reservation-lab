3e3fd9434bb016b3ef328ed91d3c2b266fad9d44
Dakota <dakota.vradenburg@gmail.com>
2018-10-22 21:21:56 -0400

    Added functionality for the table. Fixed buttons and lab completed.

--- scripts/script.js
+++ scripts/script.js
@@ -1,20 +1,40 @@
 "use strict";
 $(document).ready(() => {
-    $(".table").on("mouseover", (event) => {
+    // Hover event for tables
+    $(".available").on("mouseover", (event) => {
         $(event.target).fadeTo(500, 0.5);
     });
-
-    $(".table").on("mouseout", (event) => {
+    $(".available").on("mouseout", (event) => {
         $(event.target).fadeTo(500, 1);
     });
 
+    // Available table - display reservation form, activate save button
+    $(".available").on("click", (event) => {
+        $(".res-form").css("display", "flex");
+        saveRes(event.target);
+    });
 
+    // Save button submit - hide form, add reserved, remove available
+    function saveRes(tableClicked) {
+        // Change subtitle to current table number
+        let tableNum = $(tableClicked).text();
+        $(".res-sub-title").text(`Table Number: ${tableNum}`);
 
+        $(".res-save").on("click", () => {
+            $(".res-form").css("display", "none");
+            $(tableClicked).addClass("reserved")
+                .removeClass("available")
+                .unbind("mouseover")
+                .unbind("click");
+        });
+    }
 
-
-
-
-
+    // When "x" is pressed on form, close form and reset save button
+    $("#close-form").on("click", () => {
+        $(".res-form").css("display", "none");
+        $(".res-save").unbind("click");
+    });
+    
 
 
 
